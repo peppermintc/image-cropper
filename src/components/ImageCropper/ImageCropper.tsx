496b631c06dc6770bdcae83c4fe525e2a2e4b343
peppermintc <haminjp@naver.com>
2022-01-19 21:38:20 +0900

    set canvas size on image load

--- src/components/ImageCropper/ImageCropper.tsx
+++ src/components/ImageCropper/ImageCropper.tsx
@@ -10,7 +10,38 @@ interface Props {
   } | null;
 }
 
+interface ImageSizeInterface {
+  width: number;
+  height: number;
+}
+
 const ImageCropper: React.FC<Props> = ({ currentImage }) => {
+  const onImageLoad = () => {
+    if (!currentImage) return;
+
+    const getImageSize = (): ImageSizeInterface | undefined => {
+      const imageElement: HTMLImageElement | null =
+        document.querySelector("#image-element");
+      if (!imageElement) return;
+      const ImagePositionInfo = imageElement.getBoundingClientRect();
+      const width = ImagePositionInfo.width;
+      const height = ImagePositionInfo.height;
+      return { width, height };
+    };
+
+    const setCanvasSize = (newWidth: number, newHeight: number) => {
+      const canvasElement: HTMLCanvasElement | null =
+        document.querySelector("#canvas-element");
+      if (!canvasElement) return;
+      canvasElement.width = newWidth;
+      canvasElement.height = newHeight;
+    };
+
+    const imageSize: ImageSizeInterface | undefined = getImageSize();
+    if (!imageSize) return;
+    setCanvasSize(imageSize.width, imageSize.height);
+  };
+
   return (
     <React.Fragment>
       {currentImage ? (
@@ -18,10 +49,13 @@ const ImageCropper: React.FC<Props> = ({ currentImage }) => {
           <div className="image-name">{currentImage.name}</div>
           <div className="original-image-container">
             <img
+              id="image-element"
               className="original-image"
               src={currentImage.url}
               alt="original"
+              onLoad={onImageLoad}
             />
+            <canvas id="canvas-element"></canvas>
           </div>
         </div>
       ) : (
