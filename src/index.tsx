bee1358f6b7dfffb40b68a0eb78c06b4179f1a28
peppermintc <haminjp@naver.com>
2022-01-20 16:33:16 +0900

    github pages

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const store = createStore(
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
