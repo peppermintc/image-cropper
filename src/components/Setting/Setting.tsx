d2671efb3c0581ef271a320622f902bb3184291f
peppermintc <haminjp@naver.com>
2022-01-20 17:25:55 +0900

    add button group

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -3,17 +3,28 @@ import "./Setting.scss";
 const Setting = () => {
   return (
     <div className="setting-container">
-      <div className="save-header">Save</div>
       <div className="save-content">
         <div className="save-content-header">Individual Image</div>
         <button>Apply crop</button>
-        <button>Download cropped Image</button>
-        <button>Download original Image</button>
+        <div className="input-button-group">
+          <input
+            className="image-name-input"
+            type="text"
+            placeholder="Cropped image name"
+          />
+          <button>Download cropped Image</button>
+        </div>
+        <div className="input-button-group">
+          <input
+            className="image-name-input"
+            type="text"
+            placeholder="Original image name"
+          />
+          <button>Download original Image</button>
+        </div>
         <div className="save-content-header">All Images</div>
         <button>Download all cropped Image</button>
       </div>
-      <div className="setting-header">Setting</div>
-      <div>-</div>
     </div>
   );
 };
