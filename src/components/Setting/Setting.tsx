5b03f2c29726a6e44af2cb7f9525f4b029325789
peppermintc <haminjp@naver.com>
2022-01-20 16:59:02 +0900

    save menu update

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -5,9 +5,12 @@ const Setting = () => {
     <div className="setting-container">
       <div className="save-header">Save</div>
       <div className="save-content">
+        <div className="save-content-header">Individual Image</div>
         <button>Apply crop</button>
         <button>Download cropped Image</button>
         <button>Download original Image</button>
+        <div className="save-content-header">All Images</div>
+        <button>Download all cropped Image</button>
       </div>
       <div className="setting-header">Setting</div>
       <div>-</div>
